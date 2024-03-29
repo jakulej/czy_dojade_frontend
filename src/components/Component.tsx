072142fe3b774650300815ef6a240fa80c91260a
Wojciech Makuch <wojmak0@gmail.com>
2024-03-29 14:01:05 +0100

    init

---
+++ src/components/Component.tsx
@@ -0,0 +1,17 @@
+interface MyButtonProps {
+    title: string;
+    disabled: boolean;
+}
+
+function MyButton({ title, disabled }: MyButtonProps) {
+    return <button disabled={disabled}>{title}</button>;
+}
+
+export default function Component() {
+    return (
+        <div>
+            <h1>Welcome to my app</h1>
+            <MyButton title="I'm a disabled button" disabled={true} />
+        </div>
+    );
+}
