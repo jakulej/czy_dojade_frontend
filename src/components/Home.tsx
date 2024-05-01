9bca1d6c2f00bf109db847eebda91a700684e422
Kulej <jkul@thaumatec.com>
2024-05-01 10:15:42 +0200

    Test

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,11 @@
+import Login from "./LoginView/Login";
+
+
 export default function Home() {
     return (
         <div>
             <h1>Welcome home</h1>
+            <Login/>
         </div>
     );
 }
